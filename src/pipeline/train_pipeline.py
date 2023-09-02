effc3dc90714226e70118c46f72b602c737c3a67
allmeidaapedro <pedrooalmeida.net@gmail.com>
2023-09-02 16:46:12 -0300

    Train pipeline

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -8,41 +8,91 @@ from src.logger import logging
 from src.exception import CustomException
 
 # Components for data ingestion, transformation and model training.
-from src.components import data_ingestion, data_transformation, model_trainer
 
+# Data ingestion.
+from src.components.data_ingestion import DataIngestionConfig
+from src.components.data_ingestion import DataIngestion
+
+# Data transformation.
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+# Model trainer.
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
+
+
+class TrainPipeline:
+    '''TrainPipeline class for training a machine learning pipeline.
+
+    This class handles the training pipeline, including data ingestion,
+    data transformation, and model training.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__: Constructor method to initialize the class.
+        train: Method to execute the training pipeline.
+
+    '''
+
+    def __init__(self) -> None:
+        pass
+
+    def train(self):
+        '''Execute the training pipeline.
+
+        This method performs the following steps:
+        1. Data ingestion to obtain training and testing datasets.
+        2. Data transformation for preprocessing.
+        3. Model training using the best hyperparameters.
+
+        It also prints the classification report and ROC-AUC score.
+
+        Raises:
+            CustomException: If an error occurs during the training pipeline.
+
+        '''
+
+        try:
+            logging.info('Train full pipeline started.')
+
+            logging.info('Splitting the data into train and test sets with data_ingestion component.')
+
+            data_ingestion = DataIngestion()
+            train, test = data_ingestion.apply_data_ingestion()
+
+            logging.info('Train and test entire sets obtained (artifacts).')
+
+            logging.info('Applying all the preprocessing steps required with data_transformation component.')
+            
+            data_transformation = DataTransformation()
+            train_prepared, test_prepared, _ = data_transformation.apply_data_transformation(train, test)
+
+            logging.info('Train and test entire prepared sets obtained.')
+
+            logging.info('Train and save the best model using the best hyperparameters found during the modelling notebook analysis using model_trainer component.')
+            
+            model_trainer = ModelTrainer()
+
+            logging.info('Final best model obtained (artifacts).')
+
+            class_report, auc_score = model_trainer.apply_model_trainer(train_prepared, test_prepared)
+            print('Final model classification report:')
+            print(f'\n{class_report}')
+            print(f'\nFinal model roc-auc score:')
+            print(auc_score)
+
+            logging.info('Classification report and ROC-AUC score presented.')
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
 
 if __name__ == '__main__':
-    try:
-        
-        logging.info('Train full pipeline started.')
 
-        logging.info('Splitting the data into train and test sets with data_ingestion component.')
-
-        data_ingestion = DataIngestion()
-        train, test = data_ingestion.apply_data_ingestion()
-
-        logging.info('Train and test entire sets obtained (artifacts).')
-
-        logging.info('Applying all the preprocessing steps required with data_transformation component.')
-        
-        data_transformation = DataTransformation()
-        train_prepared, test_prepared, _ = data_transformation.apply_data_transformation(train, test)
-
-        logging.info('Train and test entire prepared sets obtained.')
-
-        logging.info('Train and save the best model using the best hyperparameters found during the modelling notebook analysis using model_trainer component.')
-        
-        model_trainer = ModelTrainer()
-
-        logging.info('Final best model obtained (artifacts).')
-
-        class_report, auc_score = model_trainer.apply_model_trainer(train_prepared, test_prepared)
-        print('Final model classification report:')
-        print(f'\n{class_report}')
-        print(f'\nFinal model roc-auc score:')
-        print(auc_score)
-
-        logging.info('Classification report and ROC-AUC score presented.')
-
-    except Exception as e:
-        raise CustomException(e, sys)
+    # Apply full train pipeline using the components above.
+    train_pipeline = TrainPipeline()
+    train_pipeline.train()
