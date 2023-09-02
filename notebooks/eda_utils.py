a506c24f99d2edd8fe2459e6a9a4a7e336ced7f6
allmeidaapedro <pedrooalmeida.net@gmail.com>
2023-09-02 16:26:04 -0300

    Modelling and eda utils

--- notebooks/eda_utils.py
+++ notebooks/eda_utils.py
@@ -7,10 +7,17 @@ This script aims to provide functions that will turn the exploratory data analys
 Importing libraries
 '''
 
+# Data manipulation and visualization.
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+# Debugging.
+from src.exception import CustomException
+import sys
+
+# Warnings.
 from warnings import filterwarnings
 filterwarnings('ignore')
 
@@ -38,36 +45,41 @@ def sns_plots(data, features, histplot=True, countplot=False,
     Returns:
         None
     """
-    num_features = len(features)
-    num_rows = num_features // 3 + (num_features % 3 > 0)  
-
-    fig, axes = plt.subplots(num_rows, 3, figsize=(20, 5*num_rows))  
-
-    for i, feature in enumerate(features):
-        row = i // 3  
-        col = i % 3  
-
-        ax = axes[row, col] if num_rows > 1 else axes[col] 
-        
-        if countplot:
-            sns.countplot(data=data, x=feature, hue=hue, ax=ax)
-        elif barplot:
-            sns.barplot(data=data, x=feature, y=barplot_y, hue=hue, ax=ax)
-        elif boxplot:
-            sns.boxplot(data=data, x=boxplot_x, y=feature, showfliers=outliers, ax=ax)
-        elif outliers:
-            sns.boxplot(data=data, x=feature, ax=ax)
-        else:
-            sns.histplot(data=data, x=feature, hue=hue, kde=kde, ax=ax)
-
-        ax.set_title(feature)  
-        ax.set_xlabel('')  
     
-    if num_features < len(axes.flat):
-        for j in range(num_features, len(axes.flat)):
-            fig.delaxes(axes.flat[j])
+    try:
+        num_features = len(features)
+        num_rows = num_features // 3 + (num_features % 3 > 0)  
 
-    plt.tight_layout()
+        fig, axes = plt.subplots(num_rows, 3, figsize=(20, 5*num_rows))  
+
+        for i, feature in enumerate(features):
+            row = i // 3  
+            col = i % 3  
+
+            ax = axes[row, col] if num_rows > 1 else axes[col] 
+            
+            if countplot:
+                sns.countplot(data=data, x=feature, hue=hue, ax=ax)
+            elif barplot:
+                sns.barplot(data=data, x=feature, y=barplot_y, hue=hue, ax=ax)
+            elif boxplot:
+                sns.boxplot(data=data, x=boxplot_x, y=feature, showfliers=outliers, ax=ax)
+            elif outliers:
+                sns.boxplot(data=data, x=feature, ax=ax)
+            else:
+                sns.histplot(data=data, x=feature, hue=hue, kde=kde, ax=ax)
+
+            ax.set_title(feature)  
+            ax.set_xlabel('')  
+        
+        if num_features < len(axes.flat):
+            for j in range(num_features, len(axes.flat)):
+                fig.delaxes(axes.flat[j])
+
+        plt.tight_layout()
+    
+    except Exception as e:
+        raise CustomException(e, sys)
 
 
 def check_outliers(data, features):
@@ -95,31 +107,36 @@ def check_outliers(data, features):
     Example:
         outlier_indexes, outlier_counts, total_outliers = check_outliers(data=my_data, features=['Age', 'Income'])
     """
-    
-    outlier_counts = {}
-    outlier_indexes = {}
-    total_outliers = 0
-    
-    for feature in features:
-        Q1 = data[feature].quantile(0.25)
-        Q3 = data[feature].quantile(0.75)
-        
-        IQR = Q3 - Q1
-        
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        
-        feature_outliers = data[(data[feature] < lower_bound) | (data[feature] > upper_bound)]
-        outlier_indexes[feature] = feature_outliers.index.tolist()
-        outlier_count = len(feature_outliers)
-        outlier_counts[feature] = outlier_count
-        total_outliers += outlier_count
-    
-    print(f'There are {total_outliers} outliers in the dataset.')
-    print()
-    print(f'Number (percentage) of outliers per feature: ')
-    print()
-    for feature, count in outlier_counts.items():
-        print(f'{feature}: {count} ({round(count/len(data)*100, 2)})%')
 
-    return outlier_indexes, outlier_counts, total_outliers
+    try:
+    
+        outlier_counts = {}
+        outlier_indexes = {}
+        total_outliers = 0
+        
+        for feature in features:
+            Q1 = data[feature].quantile(0.25)
+            Q3 = data[feature].quantile(0.75)
+            
+            IQR = Q3 - Q1
+            
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+            
+            feature_outliers = data[(data[feature] < lower_bound) | (data[feature] > upper_bound)]
+            outlier_indexes[feature] = feature_outliers.index.tolist()
+            outlier_count = len(feature_outliers)
+            outlier_counts[feature] = outlier_count
+            total_outliers += outlier_count
+        
+        print(f'There are {total_outliers} outliers in the dataset.')
+        print()
+        print(f'Number (percentage) of outliers per feature: ')
+        print()
+        for feature, count in outlier_counts.items():
+            print(f'{feature}: {count} ({round(count/len(data)*100, 2)})%')
+
+        return outlier_indexes, outlier_counts, total_outliers
+    
+    except Exception as e:
+        raise CustomException(e, sys)
--- notebooks/modelling_utils.py
+++ notebooks/modelling_utils.py
@@ -17,6 +17,10 @@ from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix, roc_curve, precision_recall_curve
 import time
 
+# Debugging.
+from src.exception import CustomException
+import sys
+
 # Warnings.
 from warnings import filterwarnings
 filterwarnings('ignore')
@@ -38,86 +42,89 @@ def evaluate_models_cv(models, X_train, y_train):
     pd.DataFrame: A DataFrame containing the evaluation results for each model, including their average validation scores
                   and training scores.
     '''
+    try:
+        # Stratified KFold in order to maintain the target proportion on each validation fold - dealing with imbalanced target.
+        n_folds = 5
+        stratified_kfold = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=42)
+
+        # Dictionaries with validation and training scores of each model for plotting further.
+        models_val_scores = dict()
+        models_train_scores = dict()
+
+        for model in models:
+            # Getting the model object from the key with his name.
+            model_instance = models[model]
+
+            # Measuring training time.
+            start_time = time.time()
+            
+            # Fitting the model to the training data.
+            model_instance.fit(X_train, y_train)
+
+            end_time = time.time()
+            training_time = end_time - start_time
+
+            # Make predictions on training data and evaluate them.
+            y_train_pred = model_instance.predict(X_train)
+            train_score = roc_auc_score(y_train, y_train_pred)
+
+            # Evaluate the model using k-fold cross validation, obtaining a robust measurement of its performance on unseen data.
+            val_scores = cross_val_score(model_instance, X_train, y_train, scoring='roc_auc', cv=stratified_kfold)
+            avg_val_score = val_scores.mean()
+            val_score_std = val_scores.std()
+
+            # Adding the model scores to the validation and training scores dictionaries.
+            models_val_scores[model] = avg_val_score
+            models_train_scores[model] = train_score
+
+            # Printing the results.
+            print(f'{model} results: ')
+            print('-'*50)
+            print(f'Training score: {train_score}')
+            print(f'Average validation score: {avg_val_score}')
+            print(f'Standard deviation: {val_score_std}')
+            print(f'Training time: {round(training_time, 5)} seconds')
+            print()
+
+        # Plotting the results.
+        print('Plotting the results: ')
+
+        # Converting scores to a dataframe
+        val_df = pd.DataFrame(list(models_val_scores.items()), columns=['Model', 'Average Val Score'])
+        train_df = pd.DataFrame(list(models_train_scores.items()), columns=['Model', 'Train Score'])
+        eval_df = val_df.merge(train_df, on='Model')
+
+        # Plotting each model and their train and validation (average) scores.
+        plt.figure(figsize=(15, 6))
+        width = 0.35
+
+        x = np.arange(len(eval_df['Model']))
+
+        val_bars = plt.bar(x - width/2, eval_df['Average Val Score'], width, label='Average Validation Score', color='skyblue')
+        train_bars = plt.bar(x + width/2, eval_df['Train Score'], width, label='Train Score', color='orange')
+
+        plt.xlabel('Model')
+        plt.ylabel('ROC-AUC Score')
+        plt.title('Models Performances')
+        plt.xticks(x, eval_df['Model'], rotation=45)
+
+        # Add scores on top of each bar
+        for bar in val_bars + train_bars:
+            height = bar.get_height()
+            plt.annotate('{}'.format(round(height, 2)),
+                        xy=(bar.get_x() + bar.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+        return eval_df
     
-    # Stratified KFold in order to maintain the target proportion on each validation fold - dealing with imbalanced target.
-    n_folds = 5
-    stratified_kfold = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=42)
-
-    # Dictionaries with validation and training scores of each model for plotting further.
-    models_val_scores = dict()
-    models_train_scores = dict()
-
-    for model in models:
-        # Getting the model object from the key with his name.
-        model_instance = models[model]
-
-        # Measuring training time.
-        start_time = time.time()
-        
-        # Fitting the model to the training data.
-        model_instance.fit(X_train, y_train)
-
-        end_time = time.time()
-        training_time = end_time - start_time
-
-        # Make predictions on training data and evaluate them.
-        y_train_pred = model_instance.predict(X_train)
-        train_score = roc_auc_score(y_train, y_train_pred)
-
-        # Evaluate the model using k-fold cross validation, obtaining a robust measurement of its performance on unseen data.
-        val_scores = cross_val_score(model_instance, X_train, y_train, scoring='roc_auc', cv=stratified_kfold)
-        avg_val_score = val_scores.mean()
-        val_score_std = val_scores.std()
-
-        # Adding the model scores to the validation and training scores dictionaries.
-        models_val_scores[model] = avg_val_score
-        models_train_scores[model] = train_score
-
-        # Printing the results.
-        print(f'{model} results: ')
-        print('-'*50)
-        print(f'Training score: {train_score}')
-        print(f'Average validation score: {avg_val_score}')
-        print(f'Standard deviation: {val_score_std}')
-        print(f'Training time: {round(training_time, 5)} seconds')
-        print()
-
-    # Plotting the results.
-    print('Plotting the results: ')
-
-    # Converting scores to a dataframe
-    val_df = pd.DataFrame(list(models_val_scores.items()), columns=['Model', 'Average Val Score'])
-    train_df = pd.DataFrame(list(models_train_scores.items()), columns=['Model', 'Train Score'])
-    eval_df = val_df.merge(train_df, on='Model')
-
-    # Plotting each model and their train and validation (average) scores.
-    plt.figure(figsize=(15, 6))
-    width = 0.35
-
-    x = np.arange(len(eval_df['Model']))
-
-    val_bars = plt.bar(x - width/2, eval_df['Average Val Score'], width, label='Average Validation Score', color='skyblue')
-    train_bars = plt.bar(x + width/2, eval_df['Train Score'], width, label='Train Score', color='orange')
-
-    plt.xlabel('Model')
-    plt.ylabel('ROC-AUC Score')
-    plt.title('Models Performances')
-    plt.xticks(x, eval_df['Model'], rotation=45)
-
-    # Add scores on top of each bar
-    for bar in val_bars + train_bars:
-        height = bar.get_height()
-        plt.annotate('{}'.format(round(height, 2)),
-                    xy=(bar.get_x() + bar.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-
-    return eval_df
+    except Exception as e:
+        raise CustomException(e, sys)
 
 
 def evaluate_classifier(y_true, y_pred):
@@ -134,31 +141,36 @@ def evaluate_classifier(y_true, y_pred):
     Returns:
     None
     '''
-    
-    # Classification report
-    print(classification_report(y_true=y_true, y_pred=y_pred))
-    
-    # Confusion matrix
-    cm = confusion_matrix(y_true=y_true, y_pred=y_pred)
-    sns.heatmap(cm, annot = True, fmt = 'd')
-    plt.title('Confusion Matrix')
-    plt.xlabel('Predicted Values')
-    plt.ylabel('Real Values')
-    plt.show()
-    
-    # ROC AUC Curve and score
-    fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_pred)
-    auc = roc_auc_score(y_true=y_true, y_score=y_pred)
 
-    plt.figure(figsize=(5, 3))
-    plt.plot(fpr, tpr, label=f'AUC = {auc:.2f}')
-    plt.plot([0, 1], [0, 1], linestyle='--', color='gray')  # Random guessing line
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend()
-    plt.show()
+    try:
+    
+        # Classification report
+        print(classification_report(y_true=y_true, y_pred=y_pred))
+        
+        # Confusion matrix
+        cm = confusion_matrix(y_true=y_true, y_pred=y_pred)
+        sns.heatmap(cm, annot = True, fmt = 'd')
+        plt.title('Confusion Matrix')
+        plt.xlabel('Predicted Values')
+        plt.ylabel('Real Values')
+        plt.show()
+        
+        # ROC AUC Curve and score
+        fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_pred)
+        auc = roc_auc_score(y_true=y_true, y_score=y_pred)
 
+        plt.figure(figsize=(5, 3))
+        plt.plot(fpr, tpr, label=f'AUC = {auc:.2f}')
+        plt.plot([0, 1], [0, 1], linestyle='--', color='gray')  # Random guessing line
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic (ROC) Curve')
+        plt.legend()
+        plt.show()
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 
 def plot_feature_importances(model, data):
     '''
@@ -175,26 +187,30 @@ def plot_feature_importances(model, data):
     None
     '''
     
-    # Get feature importances
-    importances = model.feature_importances_
-    feature_names = data.columns 
+    try:
+        # Get feature importances
+        importances = model.feature_importances_
+        feature_names = data.columns 
 
 
-    # Sort feature importances in descending order
-    indices = np.argsort(importances)[::-1]
-    sorted_feature_names = [feature_names[i] for i in indices]
-    sorted_importances = importances[indices]
+        # Sort feature importances in descending order
+        indices = np.argsort(importances)[::-1]
+        sorted_feature_names = [feature_names[i] for i in indices]
+        sorted_importances = importances[indices]
 
-    # Plot feature importances
-    color_sequence = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
-                      '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22']
+        # Plot feature importances
+        color_sequence = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+                        '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22']
 
-    plt.figure(figsize=(12, 3))
-    plt.title('Feature Importances')
-    plt.bar(range(len(importances)), sorted_importances, tick_label=sorted_feature_names, color=color_sequence)
-    plt.xticks(rotation=90)
-    plt.show()
+        plt.figure(figsize=(12, 3))
+        plt.title('Feature Importances')
+        plt.bar(range(len(importances)), sorted_importances, tick_label=sorted_feature_names, color=color_sequence)
+        plt.xticks(rotation=90)
+        plt.show()
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 
 def precision_vs_recall_curve(y_true, predicted_probas):
     '''
@@ -209,19 +225,23 @@ def precision_vs_recall_curve(y_true, predicted_probas):
     :rtype: tuple
     '''
 
-    # Obtain precision and recall values (arrays) for different threshold
-    # values, when compared to the model's estimated probability of being positive.
-    precision, recall, threshold = precision_recall_curve(y_true, predicted_probas[:, 1])
+    try:
+        # Obtain precision and recall values (arrays) for different threshold
+        # values, when compared to the model's estimated probability of being positive.
+        precision, recall, threshold = precision_recall_curve(y_true, predicted_probas[:, 1])
 
-    # Plotting precision-recall curve.
-    plt.title('Precision vs Recall Curve')
-    plt.plot(threshold, precision[:-1], 'b--', label='Precision')
-    plt.plot(threshold, recall[:-1], 'r--', label='Recall')
-    plt.xlabel('Threshold')
-    plt.legend(loc='lower left')
-    plt.ylim([0,1])
+        # Plotting precision-recall curve.
+        plt.title('Precision vs Recall Curve')
+        plt.plot(threshold, precision[:-1], 'b--', label='Precision')
+        plt.plot(threshold, recall[:-1], 'r--', label='Recall')
+        plt.xlabel('Threshold')
+        plt.legend(loc='lower left')
+        plt.ylim([0,1])
 
-    return precision, recall, threshold
+        return precision, recall, threshold
+
+    except Exception as e:
+        raise CustomException(e, sys)
 
 
 def get_threshold_metrics(precision, recall, threshold, target_metric, target_metric_value):
@@ -240,20 +260,25 @@ def get_threshold_metrics(precision, recall, threshold, target_metric, target_me
     :rtype: tuple
     '''
     
-    if target_metric == 'recall':
-        recall_array = np.asarray(recall)
-        target_value_index = np.where(recall_array[:-1] == target_metric_value)[0][0]
+    try:
 
-    else:
-        precision_array = np.asarray(precision)
-        target_value_index = np.where(precision_array[:-1] == target_metric_value)[0][0]
-    
-    threshold_precision = precision[target_value_index]
-    threshold_recall = recall[target_value_index]
-    threshold_selected = threshold[target_value_index]
+        if target_metric == 'recall':
+            recall_array = np.asarray(recall)
+            target_value_index = np.where(recall_array[:-1] == target_metric_value)[0][0]
 
-    print(f'For a threshold of {threshold_selected}:')
-    print(f'Recall: {threshold_recall}')
-    print(f'Precision: {threshold_precision}')
+        else:
+            precision_array = np.asarray(precision)
+            target_value_index = np.where(precision_array[:-1] == target_metric_value)[0][0]
+        
+        threshold_precision = precision[target_value_index]
+        threshold_recall = recall[target_value_index]
+        threshold_selected = threshold[target_value_index]
 
-    return threshold_precision, threshold_recall, threshold_selected
+        print(f'For a threshold of {threshold_selected}:')
+        print(f'Recall: {threshold_recall}')
+        print(f'Precision: {threshold_precision}')
+
+        return threshold_precision, threshold_recall, threshold_selected
+
+    except Exception as e:
+        raise CustomException(e, sys)
